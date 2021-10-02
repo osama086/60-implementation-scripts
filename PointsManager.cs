6037917007b54be5bece2925207917f0c43b55f9
Osama Adil <35128209+osama086@users.noreply.github.com>
2021-10-02 15:33:21 +0500

    Add files via upload

---
+++ PointsManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PointsManager : MonoBehaviour
+{
+    public static int numberOfPoints;
+    public Text PointsText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        numberOfPoints = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PointsText.text = " " + numberOfPoints;
+    }
+}
---
+++ RandomPoints.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomPoints : MonoBehaviour
+{
+	public GameObject things;
+	public float delayTimer = 0.5f;
+	float timer;
+
+
+	// Use this for initialization
+	void Start()
+	{
+		timer = delayTimer;
+
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+		timer -= Time.deltaTime;
+
+		if (timer <= 0)
+		{
+			Vector3 thingsPos = new Vector3(Random.Range(2.5f, -2.5f), transform.position.y, transform.position.z);
+
+			Instantiate(things, thingsPos, transform.rotation);
+			timer = delayTimer;
+		}
+	}
+}
